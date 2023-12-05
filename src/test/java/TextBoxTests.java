85d0f68e612f70eb30403532d4b79fa203ba774b
Elmira Shaykhattarova <elmirailgizovna@gmail.com>
2023-12-06 01:21:15 +0300

    Add attach

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -1,6 +1,7 @@
 //package src.test;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -25,11 +26,13 @@ public class TextBoxTests {
         SelenideLogger.addListener("AllureSelenide",new AllureSelenide());
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
-@AfterEach
-    void addAttachments(){
-
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
-
 
     @Test
     void fillFormTest() {
